b7d2f1470b4c94ae54f28e2da1ffd52aa05d56f1
Alexander Drozdov <snosme@gmail.com>
2020-08-30 16:29:50 +0300

    update internal serializer

--- src/app/exporters/internal.ts
+++ src/app/exporters/internal.ts
@@ -1,18 +1,36 @@
 import { Header } from '../viewer/headers'
 import { getNamePart } from '../dat/dat-file'
+import { ColumnStats } from '../dat/analysis'
+
+interface DatSerializedHeader {
+  name: string | null
+  length?: number
+  type: {
+    ref?: { array: boolean }
+    boolean?: {}
+    integer?: { unsigned: boolean, size: number }
+    decimal?: { size: number }
+    string?: {}
+    key?: { foreign: boolean }
+  }
+}
 
 export function exportInternalState (headers: Header[], path: string) {
-  headers = headers.filter(({ type }) => type.boolean || type.decimal || type.integer || type.string || type.key)
-
   return JSON.stringify({
     name: getNamePart(path),
-    portable: false, // @TODO: if all types defined before last size_t member, then it's portable schema
-    sizeType: 32, // portable schema must be saved with size_t = 64
-    headers: headers.map(header => ({
-      name: header.name,
-      offset: header.offset,
-      length: header.length,
-      type: header.type
-    }))
+    headers: serializeHeaders(headers)
   }, null, 2)
+}
+
+function serializeHeaders (headers: Header[]) {
+  return headers.map<DatSerializedHeader>(header => ({
+      name: header.name,
+    length: (header.type.ref || header.type.key) ? undefined : header.length,
+    type: {
+      ...header.type,
+      byteView: undefined
+    }
+    }))
+}
+
 }
