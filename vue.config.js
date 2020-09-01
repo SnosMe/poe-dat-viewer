34beb93fc403ec57f23d4a801e6bc5f405a1e116
Alexander Drozdov <snosme@gmail.com>
2020-09-01 12:23:01 +0300

    fix path

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,9 @@
 const webpack = require('webpack')
 
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/poe-dat-viewer/'
+    : '/',
   pluginOptions: {
     quasar: {
       importStrategy: 'manual',
