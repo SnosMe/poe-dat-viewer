d58b538b4afb6f34bffc7a2415f27f2cdc11d98c
Alexander Drozdov <snosme@gmail.com>
2020-10-24 19:23:49 +0300

    use own virtualscroll

--- src/app/ContentTree.vue
+++ src/app/ContentTree.vue
@@ -1,28 +1,36 @@
 <template>
-  <q-scroll-area
-    style="width: 300px; flex-shrink: 0;" class="shadow-1 q-pa-xs"
-    id="content-tree-fancy-scroll"
-  >
-    <q-virtual-scroll
+  <div class="shadow-1 layout-column" style="width: 300px; flex-shrink: 0;">
+    <virtual-scroll
+      class="flex-1"
       :items="tree"
-      :virtual-scroll-item-size="29"
-      scroll-target="#content-tree-fancy-scroll > .scroll"
-      >
-      <template v-slot="{ item }">
-        <q-btn :label="item.label"
-          flat no-caps style="font-weight: normal" dense
-          class="full-width" align="left" no-wrap
-          :icon="item.isFile ? '' : 'la la-folder-open'"
-          @click="handleFolderNav(item)" />
+      :item-height="29"
+    >
+      <template v-slot="props">
+        <div :style="{ height: props.height + 'px' }">
+          <div v-for="(item, idx) in props.items" :key="item.label"
+            style="position: absolute; width: 100%; max-width: 100%; overflow: hidden;"
+            :style="{ top: (props.top + (idx * (29))) + 'px' }"
+            class="q-pa-xs">
+            <q-btn
+              style="font-weight: normal"
+              flat no-caps dense
+              class="full-width" align="left" no-wrap
+              :label="item.label"
+              :icon="item.isFile ? '' : 'la la-folder-open'"
+              @click="handleFolderNav(item)" />
+          </div>
+        </div>
       </template>
-    </q-virtual-scroll>
-  </q-scroll-area>
+    </virtual-scroll>
+  </div>
 </template>
 
 <script>
 import { ContentTree, listDirContent, loadFile } from './patchcdn/index-store'
+import VirtualScroll from './VirtualScroll'
 
 export default {
+  components: { VirtualScroll },
   data () {
     return ContentTree
   },
