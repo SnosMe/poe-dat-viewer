c434dec892f4cc96dfcf89ca615bbdcc989b3250
Alexander Drozdov <snosme@gmail.com>
2021-06-29 10:10:20 +0300

    close button

--- viewer/src/app/workbench/Tabs.vue
+++ viewer/src/app/workbench/Tabs.vue
@@ -3,18 +3,18 @@
     <div class="flex overflow-hidden divide-x divide-gray-600">
       <div v-for="tab in tabs" :key="tab.id"
         :class="[$style.tab, { [$style.active]: tab.isActive }]">
-        <button class="pl-3 pr-1.5"
+        <button class="pl-3 h-full"
           @click="setActiveTab(tab.id)"
           @click.middle="closeTab(tab.id)">{{ tab.title }}</button>
-        <button class="pl-1.5 pr-3" title="Close"
+        <button class="mx-2 w-7 h-7 hover:bg-gray-500 text-center" title="Close"
           @click="closeTab(tab.id)"><i class="codicon codicon-close"></i></button>
       </div>
     </div>
     <div class="flex p-1.5 flex-shrink-0 space-x-1">
-      <button class="hover:bg-gray-300 hover:text-black p-2"
+      <button :class="$style.rightBtn"
         @click="openImport">Import</button>
-      <button class="hover:bg-gray-300 hover:text-black px-2" title="Help"><i class="codicon codicon-question"></i></button>
-      <button class="hover:bg-gray-300 hover:text-black px-2" title="Settings"><i class="codicon codicon-settings"></i></button>
+      <button :class="$style.rightBtn" title="Help"><i class="codicon codicon-question"></i></button>
+      <button :class="$style.rightBtn" title="Settings"><i class="codicon codicon-settings"></i></button>
     </div>
   </div>
 </template>
@@ -61,14 +61,15 @@ export default defineComponent({
   @apply text-gray-50;
   z-index: 1;
   line-height: 1;
+  min-height: 35px;
 }
 
 .tab {
   display: flex;
+  align-items: center;
 
   &:hover {
-    @apply bg-gray-300;
-    @apply text-black;
+    @apply bg-gray-600;
   }
 
   &.active {
@@ -78,6 +79,15 @@ export default defineComponent({
       @apply bg-gray-600;
       @apply text-white;
     }
+  }
+}
+
+.rightBtn {
+  @apply px-2;
+
+  &:hover {
+    @apply bg-gray-300;
+    @apply text-black;
   }
 }
 </style>
