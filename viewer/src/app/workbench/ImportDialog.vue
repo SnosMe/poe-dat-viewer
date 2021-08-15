2e96c9735b7af1c9a50d4cf77674f76642bf8c72
Alexander Drozdov <snosme@gmail.com>
2021-08-15 15:21:35 +0300

    ui

--- viewer/src/app/workbench/ImportDialog.vue
+++ viewer/src/app/workbench/ImportDialog.vue
@@ -1,38 +1,47 @@
 <template>
   <div class="p-4 overflow-auto border-l flex-1 text-base">
-    <div class="inline-flex items-baseline pb-2">
-      <div style="width: 200px;" class="my-1">
-        <input value="patchcdn.pathofexile.com/" readonly :class="$style.input" />
-        <label :class="$style.label">Patch CDN</label>
+    <div class="flex space-x-4 items-baseline mt-4">
+      <div :class="$style.importVariant">1</div>
+      <div>
+        <div class="max-w-xl mb-4 font-semibold">Useful when a new league is released. While the update is still downloading, you can review files and fix schema directly from the update servers.</div>
+        <div class="inline-flex items-baseline pb-2">
+          <div style="width: 200px;" class="my-1">
+            <input value="patchcdn.pathofexile.com/" readonly :class="$style.input" />
+            <label :class="$style.label">Patch CDN</label>
+          </div>
+          <div style="width: 100px;" class="m-1">
+            <input v-model.trim="poePatch" placeholder="3.12.x.x.x" :class="$style.input" />
+            <label :class="$style.label">Patch #</label>
+          </div>
+          <div style="width: 220px;" class="my-1">
+            <input value="/Bundles2/_.index.bin" readonly :class="$style.input" />
+            <label :class="$style.label">Index</label>
+          </div>
+          <button class="ml-1 py-1 px-3 bg-blue-600 text-white hover:bg-blue-800"
+            @click="cdnImport"
+            :disabled="isCdnImportRunning"
+            :loading="isCdnImportRunning">{{ isCdnImportRunning ? 'Wait...' : 'Import' }}</button>
+        </div>
+        <div v-if="latestPoEPatch && poePatch !== latestPoEPatch">
+          Latest PoE patch is <code class="px-1 border border-gray-300 rounded">{{ latestPoEPatch }}</code>
+        </div>
       </div>
-      <div style="width: 100px;" class="m-1">
-        <input v-model.trim="poePatch" placeholder="3.12.x.x.x" :class="$style.input" />
-        <label :class="$style.label">Patch #</label>
-      </div>
-      <div style="width: 220px;" class="my-1">
-        <input value="/Bundles2/_.index.bin" readonly :class="$style.input" />
-        <label :class="$style.label">Index</label>
-      </div>
-      <button class="ml-1 py-1 px-3 bg-blue-600 text-white hover:bg-blue-800"
-        @click="cdnImport"
-        :disabled="isCdnImportRunning"
-        :loading="isCdnImportRunning">{{ isCdnImportRunning ? 'Wait...' : 'Import' }}</button>
     </div>
-    <div v-if="latestPoEPatch && poePatch !== latestPoEPatch">
-      Latest PoE patch is <code class="px-1 border border-gray-300 rounded">{{ latestPoEPatch }}</code>
-    </div>
-    <div class="flex items-baseline mt-3 border-b pb-3">
-      <span class="pr-2">or</span>
-      <label
-        class="bg-gray-100 py-1 px-2 cursor-pointer hover:bg-gray-300"
-        style="width: 200px"
-        for="import-local-file">Pick local dat file</label>
-      <input
-        class="hidden"
-        id="import-local-file"
-        type="file"
-        accept=".dat,.dat64"
-        @input="handleFile" />
+    <div class="flex space-x-4 items-baseline mt-4 border-b pb-3">
+      <div :class="$style.importVariant">2</div>
+      <div class="flex items-baseline">
+        <span class="pr-2">or just</span>
+        <label
+          class="bg-gray-100 py-1 px-2 cursor-pointer hover:bg-gray-300"
+          style="width: 200px"
+          for="import-local-file">Pick a local dat file</label>
+        <input
+          class="hidden"
+          id="import-local-file"
+          type="file"
+          accept=".dat,.dat64"
+          @input="handleFile" />
+      </div>
     </div>
     <div class="mt-3">
       <template v-if="isFetchingSchema">
@@ -158,5 +167,15 @@ export default defineComponent({
   @apply text-xs;
   @apply px-2;
   @apply text-gray-500;
+}
+
+.importVariant {
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  line-height: 1;
+  @apply bg-gray-700 text-gray-200;
+  @apply w-8 h-8;
+  @apply rounded;
 }
 </style>
